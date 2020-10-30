c8ac7ccccf6d44e09b0f6bc2a3fb52f3a908c615
Alexander Rowland <arowland900@gmail.com>
2020-10-30 01:55:47 -0700

    add fade in for all elements

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-let name = document.querySelector('#myName')
+let name = document.querySelector('#myName');
+let choices = document.querySelector('#choices');
+let mainHeader = document.querySelector('#mainHeader');
 
 name.classList.add("fade-in")
+choices.classList.add("fade-in")
 
 setTimeout(() => {
     name.style.opacity = 1
+    choices.style.opacity = 1
+ 
 }, 2000);
+setTimeout(() => {
+    mainHeader.classList.add("fade-in")
+    setTimeout(() => {
+        mainHeader.style.opacity = 1
+    }, 2000);
+}, 1000);
