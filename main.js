413f281e3ef4c754b50b822e6e01efc7e0d4c6de
Alexander Rowland <arowland900@gmail.com>
2020-11-06 01:24:03 -0800

    add fully functional innerHTML change for slideshow

--- main.js
+++ main.js
@@ -5,7 +5,58 @@ let projectImages = [
     "images/oilytics.png",
     "images/onClick.png",
     "images/tetris.png"]
-let projectHTML = []
+let projectHTML = [
+`
+    <h1 class='projTitle'>Pygg – Project Manager</h1>             
+    <p style="font-size: 20px;">
+        - As a Lead Instructor of General Assembly's 
+        <i>Software Engineering Immersive</i> course, 
+        I oversee the production of all student projects
+        from start to finish.
+        <i>Pygg</i>, a budget tracking web application, was built 
+        by two of my former students in the Spring of 2020.                  
+    </p>
+    <p style="font-size: 20px;">
+        - <strong>Tech Stack:</strong> Django + PostgreSQL            
+    </p>
+    <p style="font-size: 20px;">- <strong> Link: </strong><a class="appLink"  href="http://pygg-app.herokuapp.com/">pygg-app.herokuapp.com</a></p>
+`, 
+`
+    <h1 class='projTitle'>Oilytics – Software Engineer</h1>             
+    <p style="font-size: 20px;">
+        - Information about Oilytics                 
+    </p>
+    <p style="font-size: 20px;">
+        - <strong>Tech Stack:</strong> React + Flask + PostgreSQL            
+    </p>
+    <p style="font-size: 20px;">- <strong> Link: </strong><a class="appLink"  href="http://pygg-app.herokuapp.com/">pygg-app.herokuapp.com</a></p>
+`, 
+`
+    <h1 class='projTitle'>OnClick – Consultant</h1>             
+    <p style="font-size: 20px;">
+        - Information about OnClick                 
+    </p>
+    <p style="font-size: 20px;">
+        - <strong>Tech Stack:</strong> GoDaddy Website Builder           
+    </p>
+    <p style="font-size: 20px;">- <strong> Link: </strong><a class="appLink"  href="http://pygg-app.herokuapp.com/">pygg-app.herokuapp.com</a></p>
+`, 
+`
+    <h1 class='projTitle'>Tetris – Project Manager</h1>             
+    <p style="font-size: 20px;">
+        - As a Lead Instructor of General Assembly's 
+        <i>Software Engineering Immersive</i> course, 
+        I oversee the production of all student projects
+        from start to finish.
+        This <i>Tetris</i> clone was built 
+        by one of my former students in the Summer of 2020.                  
+    </p>
+    <p style="font-size: 20px;">
+        - <strong>Tech Stack:</strong> HTML + CSS + JavaScript            
+    </p>
+    <p style="font-size: 20px;">- <strong> Link: </strong><a class="appLink"  href="http://pygg-app.herokuapp.com/">pygg-app.herokuapp.com</a></p>
+`
+]
 
 // CACHED ELEMENTS
 let name = document.querySelector('#myName');
@@ -24,17 +75,18 @@ pageLoaded()
 
 // EVENT LISTENERS
 footer.addEventListener('click', (e) => {
-    if(e.target.id == 'leftArrow'){
-        currentProject 
-            ? currentProject -=1
-            : currentProject = 3 
+    if (e.target.id == 'leftArrow') {
+        currentProject
+            ? currentProject -= 1
+            : currentProject = 3
     }
-    if(e.target.id == 'rightArrow') {
+    if (e.target.id == 'rightArrow') {
         currentProject == 3
             ? currentProject = 0
             : currentProject += 1
     }
     pImg.childNodes[1].src = projectImages[currentProject]
+    desc.innerHTML = projectHTML[currentProject]
 })
 
 
